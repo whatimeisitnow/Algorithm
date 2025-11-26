d0742c3ffe4454ebb51c885cce1c76faf6cce18c
whatimeisitnow <jayjay.hi.77@gmail.com>
2025-11-26 15:17:43 +0900

    problem37

--- src/main/java/com/sujin/chapter7/problem37/Main.java
+++ src/main/java/com/sujin/chapter7/problem37/Main.java
@@ -14,49 +14,38 @@ public class Main {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=new StringTokenizer(br.readLine());
 
-        int N=Integer.parseInt(st.nextToken());
         int M=Integer.parseInt(st.nextToken());
+        int N=Integer.parseInt(st.nextToken());
 
 //        System.out.println(N);
 //        System.out.println(M);
 
-        // N 이상 M이하 숫자 구하기
-        // M - N  + 1 크기의 배열(arr)선언
-        int [] arr = new int[M];
+        // N까지 넣을 수 있는 배열 생성
+        int [] arr=new int[N + 1];
 
-        // 배열에 숫자 N 부터 M까지 넣기
-        int num = 1;
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = num;
-            num ++;
+        // 2부터 N까지 배열 채우기
+        // 1은 소수 아니니깐 그냥 0으로 초기화 될 수 있도록 2부터 반복
+        for(int i = 2; i<=N; i++){
+            arr[i] = i;
         }
 
 //        System.out.println(Arrays.toString(arr));
 
-        // 배열 전체 돌면서
-        // 2부터 M/2까지 배수 있으면 0으로 바꿈
+        //제곱근까지만 반복
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            if (arr[i] == 0) continue;
 
-        if(arr[0] == 1){
-            arr[0] = 0;
-        }
-
-        for(int i=1; i<M /2 ; i++){
-            if(arr[i] == 0) continue;
-            for(int j = i + 1; j<M; j++){
-                if(arr[j] % arr[i] == 0){
-                    arr[j] = 0;
-                }
+            for (int j = i + i; j <= N; j= j + i) {
+                arr[j] = 0;
             }
         }
 
 //        System.out.println(Arrays.toString(arr));
 
-        for(int number : arr){
-            if (number != 0) {
-                System.out.println(number);
+        for (int i = M; i <= N; i++) {
+            if(arr[i] != 0){
+                System.out.println(arr[i]);
             }
         }
-
-
     }
 }
